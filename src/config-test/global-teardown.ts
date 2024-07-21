df3dd0b5a0c679a45a5cdb70f57621ab59090738
hai nguyen <hainguyen.nth11@gmail.com>
2024-07-21 20:37:32 +0700

    Remove close

--- src/config-test/global-teardown.ts
+++ src/config-test/global-teardown.ts
@@ -5,11 +5,10 @@
 // }
 
 export default async () => {
-  // @ts-expect-error - Ignore the next line TypeScript errors about 'any' type
-  if (global.browser) {
-    // @ts-expect-error - Ignore the next line TypeScript errors about 'any' type
-    await global.browser.close();
-  }
+  // if (global.browser) {
+  //   // @ts-expect-error - Ignore the next line TypeScript errors about 'any' type
+  //   await global.browser.close();
+  // }
   // console.log("Add any Teardown setup here");
   // console.log("Add any Teardown setup here2");
 };
